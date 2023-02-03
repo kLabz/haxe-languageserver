3a4d64cf2f14914c8b4f537f07407c770ae3529f
k <k@klabz.org>
2023-02-03 10:04:20 +0100

    [recording] log all watched files modifications

--- src/haxeLanguageServer/Context.hx
+++ src/haxeLanguageServer/Context.hx
@@ -408,13 +408,13 @@ class Context {
 
 	function onDidChangeWatchedFiles(event:DidChangeWatchedFilesParams) {
 		for (change in event.changes) {
+			serverRecording.onFileEvent(change);
+
 			switch change.type {
 				case Created:
-					serverRecording.onFileCreation(change);
 					callFileParamsMethod(change.uri, ServerMethods.ModuleCreated);
 				case Deleted:
 					diagnostics.clearDiagnostics(change.uri);
-					serverRecording.onFileDeletion(change);
 					callFileParamsMethod(change.uri, ServerMethods.Invalidate);
 				case _:
 			}
